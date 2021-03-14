5bc537b51dd4ccb9f00e6a6f1434c51e913d8293
Dominik Chmiel <domi_chmiel@gmx.net>
2021-03-14 16:35:04 +0100

    Simplify sleepgraph

--- sleep_graph.py
+++ sleep_graph.py
@@ -9,6 +9,8 @@ import pytz
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 import math
 
@@ -26,7 +28,7 @@ YSTEP = timedelta(minutes=6)
 DAILY_Y = int(timedelta(days=1).total_seconds() / YSTEP.total_seconds())
 
 sleep_data = np.array([[0] * days] * DAILY_Y)
-sleep_data.fill(-2)
+sleep_data.fill(0)
 
 
 def add_sleep_state(data, start, end, state):
@@ -45,74 +47,43 @@ sleep = GarminDB.Sleep.get_all(gDB)
 sleep = [x for x in sleep if x.start is not None]
 print(sleep[0])
 for sev in sleep:
-	add_sleep_state(sleep_data, sev.start, sev.end, -1.0)
+	add_sleep_state(sleep_data, sev.start, sev.end, 1)
 
 sleep_data_detail = np.array([[0] * days] * DAILY_Y)
-sleep_data_detail.fill(-2)
-sleep_data_detail = np.copy(sleep_data)
+sleep_data_detail.fill(0)
+# sleep_data_detail = np.copy(sleep_data)
 
 # Augment data with sleep Events
-print(sleep_events[0])
 for ev in sleep_events:
 	slevel = RemSleepActivityLevels[ev.event]
-	localized = tz.localize(ev.timestamp)
-	if True or slevel.value >= RemSleepActivityLevels.awake.value:
-		tofs = timedelta(hours=2)
-		if ev.timestamp > datetime(year=2019, month=10, day=27) and ev.timestamp < datetime(year=2020, month=3, day=28):
-			tofs -= timedelta(hours=1)
-		add_sleep_state(sleep_data_detail, ev.timestamp + tofs, ev.timestamp + tofs + timedelta(hours=ev.duration.hour, minutes=ev.duration.minute), slevel.value)
-
-print(sleep_data.shape)
-
-#print(sleep_events)
-
-# vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
-#               "potato", "wheat", "barley"]
-# farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
-#            "Agrifun", "Organiculture", "BioGoods Ltd.", "Cornylee Corp."]
-
-# harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
-#                     [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
-#                     [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
-#                     [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
-#                     [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
-#                     [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
-#                     [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
+	localized = ev.timestamp + tz.localize(ev.timestamp).utcoffset()
+	val = slevel.value + 1
+	#if localized.weekday() < 5:
+	#	val += 5
+	if val > 0 and localized.weekday() < 5:
+		add_sleep_state(sleep_data_detail, localized, localized + timedelta(hours=ev.duration.hour, minutes=ev.duration.minute), val)
 
 
-fig, ax = plt.subplots(2)
-ax[0].imshow(sleep_data)
-ax[1].imshow(sleep_data_detail)
+cmap = ListedColormap(["black", (0 / 256, 75 / 256, 160 / 256), (25 / 256, 118 / 256, 210 / 256), (172 / 256, 6 / 256, 188 / 256), (237 / 256, 121 / 256, 213 / 256)])
 
-y_ticks = []
-y_labels = []
-for i in range(0, 25, 4):
-	y_ticks.append(int(timedelta(hours=i) / YSTEP))
-	y_labels.append('{:2d}:00'.format(i))
+fig, ax = plt.subplots(2, gridspec_kw={'height_ratios': [1, 3]})
 
-print(y_ticks, y_labels)
-
-for a in ax:
-	a.set_yticks(y_ticks)
-	a.set_yticklabels(y_labels)
-
-# # We want to show all ticks...
-# ax.set_xticks(np.arange(len(farmers)))
-# ax.set_yticks(np.arange(len(vegetables)))
-# # ... and label them with the respective list entries
-# ax.set_xticklabels(farmers)
-# ax.set_yticklabels(vegetables)
-
-# # Rotate the tick labels and set their alignment.
-# plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-#          rotation_mode="anchor")
-
-# # Loop over data dimensions and create text annotations.
-# for i in range(len(vegetables)):
-#     for j in range(len(farmers)):
-#         text = ax.text(j, i, harvest[i, j],
-#                        ha="center", va="center", color="w")
-
-# ax.set_title("Harvest of local farmers (in tons/year)")
+for ax, dt in zip(ax, [sleep_data, sleep_data_detail]):
+	psm = ax.imshow(dt,
+		cmap=cmap,
+		rasterized=True,
+		extent=mdates.date2num([
+			date_range[0],
+			date_range[1],
+			datetime(hour=23, day=1, month=1, year=1970),
+			datetime(hour=0, day=1, month=1, year=1970)]),
+		aspect='auto'
+	)
+	fig.colorbar(psm, ax=ax)
+	ax.xaxis_date()
+	ax.yaxis_date()
+	date_format = mdates.DateFormatter('%H:%M:%S')
+	ax.yaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
 fig.tight_layout()
 plt.show()
